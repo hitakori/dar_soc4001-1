053eb950dfb298741e0a1f00352f31bf9946c09d
Mauricio Bucca <mb2322@cornell.edu>
2020-11-05 20:58:09 -0300

    Sync

--- slides/class_9/class_9.R
+++ slides/class_9/class_9.R
@@ -7,16 +7,13 @@ library("carData")
 library("janitor")
 
 data(WeightLoss)
-wl <- WeightLoss %>% select(group,starts_with("w")) %>% as_tibble(); rm(WeightLoss); wl
+wl <- WeightLoss %>% dplyr::select(group,starts_with("w")) %>% as_tibble(); rm(WeightLoss); wl
 
 
 wl %>% 
   pivot_longer(
     cols = starts_with("w")) 
 
-
-data(WeightLoss)
-wl <- WeightLoss %>% select(group,starts_with("w")) %>% as_tibble(); rm(WeightLoss); wl
 
 wl %>% 
   pivot_longer(
@@ -29,19 +26,23 @@ wl %>%
     cols = starts_with("w"), 
     names_to="week", 
     values_to= "lbs_lost",
-    names_prefix="wl") 
+    names_prefix="wl")  %>%
+    mutate(week = as.integer(week))
 
-wl %>% pivot_longer(cols = -c(id,group),     
+
+
+data(WeightLoss)
+wl <- WeightLoss  %>% as_tibble(); rm(WeightLoss); wl
+
+ 
+wl %>% mutate(id = 1:n()) %>%
+  pivot_longer(cols = -c(id,group),     
                     names_to = "outcome_week", 
                     values_to = "score") 
 
 
-wl %>% pivot_longer(cols = -c(id,group),     
-                    names_to = "outcome_week",
-                    values_to = "score")
-
-
-wl_long <- wl %>% pivot_longer(cols = -c(id,group),     
+wl_long <- wl %>% mutate(id = 1:n()) %>% 
+      pivot_longer(cols = -c(id,group),     
                                names_to = "outcome_week",
                                values_to = "score") %>%
   separate(outcome_week, into = c("outcome", "week"), sep = 2); wl_long
@@ -50,8 +51,6 @@ wl_long <- wl %>% pivot_longer(cols = -c(id,group),
 wl_long %>% 
   pivot_wider(names_from = outcome, values_from = score)
 
-
-wl_long %>% mutate(error = rnorm(n()))
 
 
 wl_long %>% mutate(error = rnorm(n())) %>%
@@ -62,48 +61,27 @@ wl_long %>% mutate(error = rnorm(n())) %>%
   pivot_wider(names_from = c(outcome,week), values_from = c(score, error))  
 
 
+##############################
+
+setwd("/Users/Mauricio/Library/Mobile Documents/com~apple~CloudDocs/Teaching/ISUC/2020_2_data_analysis_r/repo/slides/class_9")
 
 data_mpd_messy <- read_delim("mpd2020.csv", delim = ";")
 
-data_mpd <- data_mpd_messy %>% select(X1,starts_with("GDP")) %>% row_to_names(row_number = 1) %>%
+
+data_mpd_gdp <- data_mpd_messy %>% dplyr::select(X1,starts_with("GDP")) %>% row_to_names(row_number = 1) %>%
   rename(year=Region) %>% 
   filter(year!="Year") %>%
-  pivot_longer(-year, names_to= "region", values_to="population"); data_mpd
+  pivot_longer(cols = -year, names_to= "region", values_to="gdp"); data_mpd_gdp
 
 
-data_mpd <- data_mpd %>% separate(region, into = c("zone", "continent"), sep = " ", extra="merge")
-data_mpd 
-
-
-data_mpd <- data_mpd %>% unite(col = "region", c("zone","continent"), sep = "-")
-data_mpd 
-
-
-data_mpd_messy <- read_delim("mpd2020.csv", delim = ";")
-data_mpd_messy
-
-
-data_mpd_messy %>% select(X1,starts_with("GDP")) %>% row_to_names(row_number = 1) %>%
+data_mpd_pop <- data_mpd_messy %>% dplyr::select(X1,starts_with("Pop")) %>% row_to_names(row_number = 1) %>%
   rename(year=Region) %>% 
-  filter(year!="Year") 
-
-
-
-data_mpd_gpd <- data_mpd_messy %>% select(X1,starts_with("GDP")) %>% row_to_names(row_number = 1) %>%
-  rename(year=Region) %>%
   filter(year!="Year") %>%
-  pivot_longer(-year, names_to= "region", values_to="dgp"); data_mpd_gpd  data_mpd_gpd 
+  pivot_longer(cols = -year, names_to= "region", values_to="population"); data_mpd_pop
 
 
 
-data_mpd_pop <- data_mpd_messy %>% select(X1,starts_with("Population")) %>% row_to_names(row_number = 1) %>%
-  rename(year=Region) %>%
-  filter(year!="Year") %>%
-  pivot_longer(-year, names_to= "region", values_to="population"); data_mpd_pop
-
-
-
-data_mpd <- data_mpd_gpd %>% left_join(data_mpd_pop, by=c("year","region"))
+data_mpd <- data_mpd_gdp %>% left_join(data_mpd_pop, by=c("year","region"))
 data_mpd 
 
 
