f95158dd419bc0919e67a2ca5e8edfd439eb25e8
Mauricio Bucca <mb2322@cornell.edu>
2020-11-12 21:02:03 -0300

    Sync

--- slides/class_11/class_11.R
+++ slides/class_11/class_11.R
@@ -6,7 +6,8 @@ rm(list = ls())
 
 library("tidyverse")
 library("ineq")
-
+library("ggsci")
+library("viridis")
 
 setwd(
   "~/Library/Mobile Documents/com~apple~CloudDocs/Teaching/ISUC/2020_2_data_analysis_r/repo/slides/class_5/")
@@ -16,16 +17,53 @@ data_casen_csv <- read_csv("sample_casen2017.csv")
 data_casen_csv %>% glimpse()
 
 data_ineq <- data_casen_csv %>% 
-  group_by(comuna,sexo) %>% 
-  summarise(gini_ytot=Gini(ytotcor,na.rm = T), gini_yaut=Gini(yautcor,na.rm = T))
-
+  group_by(comuna,educ,sexo) %>% 
+  summarise(gini_ytot=Gini(ytotcor,na.rm = T), gini_yaut=Gini(yautcor,na.rm = T), media_yaut=mean(yautcor,na.rm = T)) %>%
+  mutate(ratio=gini_ytot/gini_yaut) %>%
+  mutate(educ = if_else(educ==99,NA_real_,educ))
 
 # Construir gráfico en layers
 
-data_ineq %>%
-  ggplot(
-    aes(x=gini_yaut, y=gini_ytot, group = factor(sexo),  colour = factor(sexo), shape = factor(sexo))) +
-   geom_point()  +
-   geom_smooth(se=F) + 
-   labs(x="Gini Ingreso autónomo indiv.",  y="Gini Ingreso total indiv.",
-        title = "Redistribución de ingresos por región", shape = "Sexo", color = "Sexo") 
+# paso 1:
+
+data_ineq %>% ggplot()
+
+#o
+
+ggplot(data=data_ineq) 
+
+
+# paso 2
+
+# color continuo
+data_ineq %>% ggplot(aes(x=media_yaut, y=gini_yaut)) + geom_point()
+                     
+g <- data_ineq %>% ggplot(aes(x=log(media_yaut), y=gini_yaut, colour=educ)) +
+  geom_point(size=2, alpha=0.4) +
+  geom_hline(yintercept = 0.5) +
+  geom_vline(xintercept = 10) +
+  ylim(0,0.7) +
+  theme_bw() +
+  facet_grid( sexo ~ .) +
+  labs(x="Log median ingreso autónomo indiv.",  y="Gini Ingreso autonomo indiv.", color = "Educación") +
+  scale_color_viridis(option="magma") 
+
+g
+data_ineq %>% ggplot(aes(x=log(media_yaut), y=gini_yaut, colour=educ)) +
+  geom_point(size=2, alpha=0.4) +
+  geom_hline(yintercept = 0.5) +
+  geom_vline(xintercept = 10) +
+  ylim(0,0.7) +
+  theme_bw() +
+  facet_grid( . ~ sexo) +
+  labs(x="Log median ingreso autónomo indiv.",  y="Gini Ingreso autonomo indiv.", color = "Educación") +
+  scale_color_viridis(option="magma") 
+
+
+# RETOMAR: jitter + transparencia
+
+data_casen_csv %>% ggplot(aes(x=esc, y=yautcor, colour=sexo)) +
+  geom_point(size=2, alpha=0.4) +
+  theme_bw() +
+  scale_color_viridis(option="magma") 
+
